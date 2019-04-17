632847bea74ff2c2ca52a86165b914ede4c79c83
Kiran Duggirala <kduggirala10@stuy.edu>
2019-04-17 10:27:11 -0400

    Wrote pushDown and swap method

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,49 @@
 
 public class MyHeap {
 	//We discussed these 2 methods already:
-	private static void pushDown(int[]data,int size,int index) {}
+	private static void swap(int[] data, int index1, int index2) {
+		int temp = data[index1];
+		data[index1] = data[index2];
+		data[index2] = temp;
+	}
+	private static void pushDown(int[]data,int size,int index) {
+		int child1, child2;
+		while (index < size) {
+			child1 = (2 * index) + 1;
+			child2 = child1 + 1;
+			if (child2 < size) {
+				if (data[child1] > data[child2]) {
+					if (data[child1] > data[index]) {
+						swap(data, child1, index);
+						index = child1;
+					}
+					else {
+						break;
+					}
+				}
+				else {
+					if (data[child2] > data[index]) {
+						swap(data, child2, index);
+						index = child2;
+					}
+					else {
+						break;
+					}
+				}
+			}
+			else {
+				if (child1 < size) {
+					if (data[child1] > data[index]) {
+						swap(data, child1, index);
+					}
+					break;
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
 	/*     - size  is the number of elements in the data array.  
 	     - push the element at index i downward into the correct position. This will swap with the larger of the child nodes provided thatchild is larger. This stops when a leaf is reached, or neither child is larger. [ should be O(logn) ]
 	     - precondition: index is between 0 and size-1 inclusive
